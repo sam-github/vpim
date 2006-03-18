d95f37199faaa28219e8e538c6f285e3bc3ffd2d
Sam Roberts <vieuxtech@gmail.com>
2006-03-18 20:16:12 +0000

    I think that the top-level vpim.rb may be required by ruby-gems.

---
+++ lib/vpim.rb
@@ -0,0 +1,14 @@
+=begin
+  Copyright (C) 2006 Sam Roberts
+
+  This library is free software; you can redistribute it and/or modify it
+  under the same terms as the ruby language itself, see the file COPYING for
+  details.
+=end
+
+# I think a file like this may be required by ruby-gems.
+
+require 'vpim/icalendar'
+require 'vpim/vcard'
+require 'vpim/maker/vcard'
+
