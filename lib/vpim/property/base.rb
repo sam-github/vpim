92d811ac1f6ec42831b232f243a92fdaee16c7fc
Sam Roberts <vieuxtech@gmail.com>
2006-03-23 07:06:21 +0000

    Pulled some classes into their own files. Asserted copyright.

--- lib/vpim/property/base.rb
+++ lib/vpim/property/base.rb
@@ -1,3 +1,11 @@
+=begin
+  Copyright (C) 2006 Sam Roberts
+
+  This library is free software; you can redistribute it and/or modify it
+  under the same terms as the ruby language itself, see the file COPYING for
+  details.
+=end
+
 module Vpim
   class Icalendar
     module Property #:nodoc:
--- lib/vpim/property/common.rb
+++ lib/vpim/property/common.rb
@@ -1,4 +1,13 @@
+=begin
+  Copyright (C) 2006 Sam Roberts
+
+  This library is free software; you can redistribute it and/or modify it
+  under the same terms as the ruby language itself, see the file COPYING for
+  details.
+=end
+
 require 'vpim/attachment'
+require 'vpim/address'
 
 module Vpim
   class Icalendar
