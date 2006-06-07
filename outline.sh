edb875e91d591e2e479483994d2b8d1860fdb16c
Sam Roberts <vieuxtech@gmail.com>
2006-06-07 02:34:54 +0000

    .

--- outline.sh
+++ outline.sh
@@ -1,2 +1,4 @@
-egrep ' *((def)|(class)|(module)|(attr_[a-z]*))\>' lib/vpim/**/*.rb | perl -ne'm/lib\/vpim\/(.*\.rb):(.*)/; print sprintf("%-25s %s\n", $1, $2);'
+#!/bin/zsh
+
+egrep '^ *((def)|(class)|(module)|(include)|(attr_[a-z]*))\>' lib/vpim/**/*.rb | perl -ne'm/lib\/vpim\/(.*\.rb):(.*)/; print sprintf("%-25s %s\n", $1, $2);' | grep -v plist
 
