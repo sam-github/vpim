369114b86921515c6e620b83743c858e3d364888
Sam Roberts <vieuxtech@gmail.com>
2009-03-10 04:23:55 +0000

    Use html4 tags, simplify structure, change to css instead of sass.

--- lib/vpim/agent/ics.rb
+++ lib/vpim/agent/ics.rb
@@ -14,20 +14,23 @@ require 'vpim/view'
 
 require 'sinatra/base'
 
-# http://agent.octetcloud.com/atom?http://www.mozilla.org/projects/calendar/caldata/CanadaHolidays.ics
-
-# TODO Rename templates to agent/ics/sass and agent/ics/haml
+# TODO Rename templates to vpim/agent/style and vpim/agent/view
 # TODO Move code into methods
+# TODO Move code into Agent base class
+# TODO Pasting of webcal links, conversion to webcal links?
+
 module Vpim
   module Agent
 
     class Ics < Sinatra::Base
       use_in_file_templates!
 
-      # TODO - move following into Agent/Servlet base class
-      def css # < Agent
-        content_type 'text/css'
-        options.templates[:"servlet/css"]
+      def css(template) # < Agent
+        render :css, template, {}
+      end
+
+      def render_css(template, data, options)
+        data
       end
 
       # Complete path, as requested by the client. Take care for CGI path rewriting.
@@ -122,91 +125,72 @@ module Vpim
 
       get '/style.css' do
         content_type 'text/css'
-        sass :"ics.sass"
+        css :"ics.css"
       end
     end
 
   end # Agent
 end # Vpim
 
-
 __END__
-@@ics.sass
-body
-  :background-color gray
-  :font-size medium
-  a
-    :color black
-    :font-style italic
-  a:hover
-    :color darkred
-
-#header
-  :border-bottom 3px solid darkred
-  #title
-    :color black
-    :font-size large
-
-.text
-  :width 80%
--#:color yellow
-
-#submit
-  :margin-top 30px
-  :margin-left 5%
-  #form
-    :padding
-      :top 10px
-      :bottom 10px
-      :left 10px
-      :right 10px
-    :text-align left
-    #url
-      :width 80%
-    #button
-      :font-weight bold
-      :text-align center
-
-#subscribe
-  :margin-left 5%
--#.feed
--#  :margin-left 10%
-  
-#footer
-  :border-top 3px solid darkred
-  :margin-top 20px
+@@ics.css
+body {
+  background-color: gray;
+}
+h1 {
+  border-bottom: 3px solid darkred;
+  font-size: large;
+}
+form {
+  margin-left: 10%;
+}
+input.text {
+  width: 80%;
+}
+a {
+  color: black;
+}
+a:hover {
+  color: darkred;
+}
+tt {
+  margin-left: 10%;
+}
+.footer {
+  border-top: 3px solid darkred;
+}
 @@ics.haml
 %html
   %head
     %title Subscribe to calendar feeds as atom feeds
     %link{:href => script_url + '/style.css', :media => 'screen', :type => 'text/css'}
   %body
-    #header
-      %span#title Subscribe to calendar feeds as atom feeds
-    #submit
-      .text Calendar feeds are great, but sometimes all you want is an atom feed of what's coming up in the next week.
-      .text Paste the URL of the calendar below, submit it, and subscribe.
-      .text= [script_url, env["SCRIPT_NAME"], env["PATH_INFO"]].inspect
-      %form#form{:method => 'POST'}
-        %input#url{:name => 'url', :value => params[:url]}
-        %input#button{:type => 'submit', :value => 'Submit'}
+    %h1 Subscribe to calendar feeds as atom feeds
+    %p
+      Calendar feeds are great, but when you want a reminder of what's coming up
+      in the next week, you might want those events as an atom feed.
+    %p
+      Paste the URL of the calendar below, submit it, and subscribe.
+    %form{:method => 'POST'}
+      %input.text{:type => 'text', :name => 'url', :value => @url_ics}
+      %input{:type => 'submit', :value => 'Submit'}
     - if @url_atom
-      #subscribe
-        .text
-          Subscribe to
-          %a{:href => @url_ics}= @url_ics
-          as:
+      %p
+        Subscribe to
+        %a{:href => @url_ics}= @url_ics
+        as:
         %ul.feed
-          %a{:href => @url_atom}= @url_atom
-          (atom feed)
+          %li
+            %a{:href => @url_atom}= @url_atom
+            (atom feed)
     - if @url_error
-      #error.text
-        #preamble Sorry, trying to access:
-        #source= @url_ics
-        #transition resulted in:
-        #destination= @url_error
-    #footer
-      .text
-        :textile
-          Coming from the "Octet Cloud":http://octetcloud.com/ using "vPim":http://vpim.rubyforge.org/, piloted by cloud monkey "Sam Roberts":mailto:vieuxtech@gmail.com
+      %p
+        Sorry, trying to access
+        %tt=@url_ics
+        resulted in:
+      %p
+        %tt= @url_error
+    .footer
+      :textile
+        Brought from the "Octet Cloud":http://octetcloud.com/ using "vPim":http://vpim.rubyforge.org/, by cloud monkey "Sam Roberts":mailto:vieuxtech@gmail.com.
 
