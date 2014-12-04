9335cb09806777a9c76d2e9752a3279e0ad4a2a1
Sam Roberts <sam@strongloop.com>
2014-12-03 21:11:23 -0800

    wip, relicense

--- lib/vpim/address.rb
+++ lib/vpim/address.rb
@@ -1,10 +1,6 @@
 # -*- encoding : utf-8 -*-
 =begin
   Copyright (C) 2008 Sam Roberts
-
-  This library is free software; you can redistribute it and/or modify it
-  under the same terms as the ruby language itself, see the file COPYING for
-  details.
 =end
 
 =begin
